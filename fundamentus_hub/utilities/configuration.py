1e1276bbfca6cc8d25b8e01b9e8f0d7089ae0399
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-10-03 18:39:12 -0300

    refactor: add option to display period

--- fundamentus_hub/utilities/configuration.py
+++ fundamentus_hub/utilities/configuration.py
@@ -45,27 +45,42 @@ class SGSAPIConfiguration(Enum):
     TAXA_SELIC = {'url': __API.format(432, __RESPONSE_FORMAT),
                   'label': ':green[Taxa Selic]',
                   'help': 'Taxa de juros - Meta Selic definida pelo Copom.',
-                  'output_format': 'percent'}
+                  'output_format': 'percent_aa'}
     # 4389 - Taxa de juros - CDI anualizada base 252.
     CDI = {'url': __API.format(4389, __RESPONSE_FORMAT),
            'label': ':green[CDI]',
            'help': 'Taxa de juros - CDI anualizada base 252.',
-           'output_format': 'percent'}
-    # 13521 - Meta para a inflação.
-    META_INFLACAO = {'url': __API.format(13521, __RESPONSE_FORMAT),
-                     'label': ':green[Meta Inflação]',
-                     'help': 'Meta para a inflação.',
-                     'output_format': 'percent'}
+           'output_format': 'percent_aa'}
+    # 226 - Taxa referencial (TR).
+    TR = {'url': __API.format(226, __RESPONSE_FORMAT),
+          'label': ':green[TR]',
+          'help': 'Taxa referencial (TR).',
+          'output_format': 'percent_am'}
+    # 256 - Taxa de juros de longo prazo - TJLP.
+    TJLP = {'url': __API.format(256, __RESPONSE_FORMAT),
+            'label': ':green[TJLP]',
+            'help': 'Taxa de juros de longo prazo - TJLP.',
+            'output_format': 'percent_aa'}
+    # 253 - Taxa básica financeira (TBF).
+    TBF = {'url': __API.format(253, __RESPONSE_FORMAT),
+           'label': ':green[TBF]',
+           'help': 'Taxa básica financeira (TBF).',
+           'output_format': 'percent_am'}
+    # 195 - Depósitos de poupança a partir de 04.05.2012 - Rentabilidade no período.
+    POUPANCA = {'url': __API.format(195, __RESPONSE_FORMAT),
+                'label': ':green[Poupança]',
+                'help': 'Depósitos de poupança a partir de 04.05.2012 - Rentabilidade no período.',
+                'output_format': 'percent_am'}
     # 433 - Índice nacional de preços ao consumidor-amplo (IPCA).
     ICPA = {'url': __API.format(433, __RESPONSE_FORMAT),
             'label': ':green[IPCA]',
             'help': 'Índice nacional de preços ao consumidor-amplo (IPCA).',
-            'output_format': 'percent'}
-    # 189 - Índice geral de preços do mercado (IGP-M)
+            'output_format': 'percent_aa'}
+    # 189 - Índice geral de preços do mercado (IGP-M).
     IGPM = {'url': __API.format(189, __RESPONSE_FORMAT),
             'label': ':green[IGP-M]',
             'help': 'Índice geral de preços do mercado (IGP-M).',
-            'output_format': 'percent'}
+            'output_format': 'percent_am'}
     # 1 - Taxa de câmbio - Livre - Dólar americano (venda) - diário.
     DOLAR = {'url': __API.format(1, __RESPONSE_FORMAT),
              'label': ':green[Dólar]',
@@ -76,8 +91,13 @@ class SGSAPIConfiguration(Enum):
             'label': ':green[Euro]',
             'help': 'Taxa de câmbio - Livre - Euro (venda).',
             'output_format': 'currency'}
-    # 7326 - Produto Interno Bruto - Taxa de variação real no ano
+    # 7326 - Produto Interno Bruto - Taxa de variação real no ano.
     PIB = {'url': __API.format(7326, __RESPONSE_FORMAT),
            'label': ':green[PIB]',
            'help': 'Produto Interno Bruto - Taxa de variação real no ano.',
            'output_format': 'percent'}
+    # 13521 - Meta para a inflação.
+    META_INFLACAO = {'url': __API.format(13521, __RESPONSE_FORMAT),
+                     'label': ':green[Meta Inflação]',
+                     'help': 'Meta para a inflação.',
+                     'output_format': 'percent_aa'}
